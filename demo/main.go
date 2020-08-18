343a0799d45c4e1d5b72aa3823adbfc806a4fd1b
keob <isgopher@outlook.com>
2020-08-18 11:39:55 +0800

    Add distance method

--- demo/main.go
+++ demo/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+
+	fmt.Println(perim.Distance())
 }
 
 func hasPrefix(s, prefix string) bool {
@@ -18,4 +31,26 @@ func contains(s, substr string) bool {
 		}
 	}
 	return false
+}
+
+type Point struct {
+	X, Y float64
+}
+
+type Path []Point
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+
+	return sum
 }
